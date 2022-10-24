8897cf3720e0bfcdf0bc8b162b5ec62b3a623653
duran.valentina@hotmail.com <duran.valentina@hotmail.com>
2022-10-24 12:21:49 -0300

    Diseño

--- RefugioPerritos/Form1.Designer.cs
+++ RefugioPerritos/Form1.Designer.cs
@@ -28,12 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btCargar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtOjos = new System.Windows.Forms.TextBox();
+            this.txtPelo = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbEdadAprox = new System.Windows.Forms.ComboBox();
+            this.btVer = new System.Windows.Forms.Button();
+            this.dgvPerros = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CARGAR PERRITOS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btCargar
+            // 
+            this.btCargar.Location = new System.Drawing.Point(408, 196);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(75, 23);
+            this.btCargar.TabIndex = 2;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            this.btCargar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(144, 84);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 23);
+            this.txtNombre.TabIndex = 3;
+            this.txtNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtOjos
+            // 
+            this.txtOjos.Location = new System.Drawing.Point(144, 231);
+            this.txtOjos.Name = "txtOjos";
+            this.txtOjos.Size = new System.Drawing.Size(100, 23);
+            this.txtOjos.TabIndex = 5;
+            // 
+            // txtPelo
+            // 
+            this.txtPelo.Location = new System.Drawing.Point(144, 188);
+            this.txtPelo.Name = "txtPelo";
+            this.txtPelo.Size = new System.Drawing.Size(100, 23);
+            this.txtPelo.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(380, 159);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "Refugios";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Edad Aproximada ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Color Pelo ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Color Ojos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(380, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Refugio";
+            // 
+            // cbEdadAprox
+            // 
+            this.cbEdadAprox.FormattingEnabled = true;
+            this.cbEdadAprox.Location = new System.Drawing.Point(144, 137);
+            this.cbEdadAprox.Name = "cbEdadAprox";
+            this.cbEdadAprox.Size = new System.Drawing.Size(121, 23);
+            this.cbEdadAprox.TabIndex = 14;
+            // 
+            // btVer
+            // 
+            this.btVer.Location = new System.Drawing.Point(408, 231);
+            this.btVer.Name = "btVer";
+            this.btVer.Size = new System.Drawing.Size(75, 23);
+            this.btVer.TabIndex = 15;
+            this.btVer.Text = "Ver Perritos";
+            this.btVer.UseVisualStyleBackColor = true;
+            // 
+            // dgvPerros
+            // 
+            this.dgvPerros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPerros.Location = new System.Drawing.Point(84, 299);
+            this.dgvPerros.Name = "dgvPerros";
+            this.dgvPerros.RowTemplate.Height = 25;
+            this.dgvPerros.Size = new System.Drawing.Size(240, 150);
+            this.dgvPerros.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvPerros);
+            this.Controls.Add(this.btVer);
+            this.Controls.Add(this.cbEdadAprox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtPelo);
+            this.Controls.Add(this.txtOjos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btCargar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Label label1;
+        private Button btCargar;
+        private TextBox txtNombre;
+        private TextBox txtOjos;
+        private TextBox txtPelo;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ComboBox cbEdadAprox;
+        private Button btVer;
+        private DataGridView dgvPerros;
     }
 }
--- RefugioPerritos/Form1.cs
+++ RefugioPerritos/Form1.cs
@@ -6,5 +6,40 @@ namespace RefugioPerritos
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
